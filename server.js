c3a3cc7fa9673c2099e6c4179614949e1fa2111d
muvaldes-afk <muvaldes@gmail.com>
2026-05-12 18:15:45 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 
 const app = express();
 const pool = new Pool({
-  user: 'gato',
+  user: 'postgres',
   host: 'localhost',
   database: 'datingapp',
   password: 'gato1331',
